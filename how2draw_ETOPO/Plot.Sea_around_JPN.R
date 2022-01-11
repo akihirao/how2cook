f176a5276f0af705ab93485cc42bb52a7ad26a89
akihirao <akihirao@gmail.com>
2022-01-11 12:12:05 +0900

    update

---
+++ how2draw_ETOPO/Plot.Sea_around_JPN.R
@@ -0,0 +1,52 @@
+#Plot.Seq_around_JPN.R
+#by Akira Hirao
+
+
+#以下のライブラリーの全ては必要ないのだけれど。。。
+library(sp)
+library(rgdal)
+library(raster)
+library(dismo)
+library(maptools)
+library(ade4)
+library(ecospat)
+library(usdm)
+library(gridExtra)
+library(ggplot2)
+library(reshape2)
+
+
+#深度/標高のカラーリング用関数
+#https://www.benjaminbell.co.uk/2019/08/bathymetric-maps-in-r-colour-palettes.html
+# Function to calculate colour break points
+# x = raster, b1 & b2 = number of divisions for each sequence, r1 & r2 = rounding value
+colbr <- function(x, b1=50, b2=50, r1=-2, r2=-2) {
+     # Min/max values of the raster (x)
+	mi <- cellStats(x, stat="min")-100
+	ma <- cellStats(x, stat="max")+100
+	# Create sequences, but only use unique numbers
+	s1 <- unique(round(seq(mi, 0, 0-mi/b1),r1))
+	s2 <- unique(round(seq(0, ma, ma/b2),r2))
+     # Combine sequence for our break points, removing duplicate 0
+	s3 <- c(s1, s2[-1])
+	# Create a list with the outputs
+     # [[1]] = length of the first sequence minus 1 (water)
+     # [[2]] = length of the second sequence minus 1 (land)
+     # [[3]] = The break points
+	x <- list(length(s1)-1, length(s2)-1, s3)
+}
+
+# Colour palette
+blue.col <- colorRampPalette(c("darkblue", "lightblue"))
+
+
+#ETOPO1: NOAAより作成されたWGS84回転楕円体に準拠した全球1分メッシュ, 格子点数4億6656個のデジタル地形データ
+#https://www.ngdc.noaa.gov/mgg/global よりDL
+#オリジナルのETOPO1_Bed_g_geotiff.tifはサイズが大きいので(466.7MB)、日本周辺域（東経115—160, 北緯25-60）のtifを使用。
+ETOPO1_geo_JPN <- raster("ETOPO1_geo_JPN.tif")
+
+#深度/標高のカラーリング用関数を適用
+ETOPO1_geo_JPN.br <- colbr(ETOPO1_geo_JPN)
+
+plot(ETOPO1_geo_JPN, col=c(blue.col(ETOPO1_geo_JPN.br[[1]]),terrain.colors(ETOPO1_geo_JPN.br[[2]])),breaks=ETOPO1_geo_JPN.br[[3]])
+
